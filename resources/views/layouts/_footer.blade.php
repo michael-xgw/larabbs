6e4d612b02efbe22623cd18b9cdd43c0f3fc8cf1
michael-xgw <1020846675@qq.com>
2020-07-08 21:13:53 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p> 由 <a href="" target="_blank">Michael</a>设计和编码</p>
+        <p class="float-right"><a href="mailto:1@qq.com">联系我们</a></p>
+    </div>
+</footer>
