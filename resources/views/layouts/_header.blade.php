14a75c82adc5359a8bd28beaa7dccab391aaadf9
michael-xgw <1020846675@qq.com>
2020-07-08 21:42:45 +0800

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,8 +11,8 @@
         <ul class="navbar-nav mr-auto"></ul>
 
         <ul class="navbar-nav navbar-right">
-            <li class="nav-item"><a href="" class="nav-link">登录</a></li>
-            <li class="nav-item"><a href="" class="nav-link">注册</a></li>
+            <li class="nav-item"><a href="{{route('login')}}" class="nav-link">登录</a></li>
+            <li class="nav-item"><a href="{{route('register')}}" class="nav-link">注册</a></li>
         </ul>
     </div>
     </div>
