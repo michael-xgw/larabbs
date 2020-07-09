a54e8b6f87dd06c903cfdae1aae3eb995e5f28cc
michael-xgw <1020846675@qq.com>
2020-07-09 21:35:41 +0800

    编辑个人资料

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="col-md-8 offset-md-2">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    <i class="glyphicon glyphicon-edit"></i>编辑个人资料
+                </h4>
+            </div>
+            <div class="card-body">
+                <form action="{{route('users.update',$user->id)}}" method="post" accept-charset="utf-8">
+                    <input type="hidden" name="_method" value="PUT">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    @include('shared._error')
+                    <div class="form-group">
+                        <label for="name-field">用户名</label>
+                        <input type="text" class="form-control" name="name" id="name-field" value="{{old('name',$user->name)}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email-field">邮箱</label>
+                        <input type="text" class="form-contrl" name="email" id="email-field" value="{{old('email',$user->email)}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="introduction-field">个人介绍</label>
+                        <textarea name="introduction" id="introduction-field" class="form-control"  rows="3" >
+                            {{old('introduction',$user->introduction)}}
+                        </textarea>
+                    </div>
+
+                    <div class="well well-sm">
+                        <button type="submit" class="btn btn-primary">提交</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,7 +9,7 @@
             <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594307452289&di=c13d035f8dddab03fbd21f75428a843e&imgtype=0&src=http%3A%2F%2Fpic0.qiyipic.com%2Fimage%2F20180425%2F1b%2Fa7%2Fv_115783662_m_601_180_101.jpg" alt="card-img-top">
             <div class="card-body">
                 <h5><strong>个人简介</strong></h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>{{$user->introduction}}</p>
                 <hr>
                 <h5><strong>注册于</strong></h5>
                 <p>January 01 1901</p>
@@ -20,7 +20,7 @@
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
         <div class="card">
             <div class="card-body">
-            <h1 class="mb-0" style="font-size:22px;">{{$user->name}}<small>{{$user->email}}</small></h1>
+            <h1 class="mb-0" style="font-size:22px;">{{$user->name}}<small>---{{$user->email}}</small></h1>
             </div>
         </div>
         <hr>
